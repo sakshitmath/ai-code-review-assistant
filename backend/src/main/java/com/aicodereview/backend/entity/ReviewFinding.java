e40d9d04293d880b3eda9482699854f63892c08c
Sakshi Torgalmath <sakshitmath@gmail.com>
2026-07-13 17:45:30 +0530

    chore: remove uploads and reports from version control

---
+++ backend/src/main/java/com/aicodereview/backend/entity/ReviewFinding.java
@@ -0,0 +1,4 @@
+package com.aicodereview.backend.entity;
+
+public class ReviewFinding {
+}
---
+++ backend/src/main/java/com/aicodereview/backend/repository/ReviewFindingRepository.java
@@ -0,0 +1,4 @@
+package com.aicodereview.backend.repository;
+
+public interface ReviewFindingRepository {
+}
